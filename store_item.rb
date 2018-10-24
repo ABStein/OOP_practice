4f1b459a3bea8b5bd0ba801f5f7f87096fabb51d
ABStein <abs19stein@gmail.com>
2018-10-23 19:20:13 -0500

    more prctice using symbols

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+motherboard = {price: 150, wifi: true, brand: "asus", stock: 4}
+cpu = {price: 300, cores: 8, multithreading: true, brand: "amd"}
+ram = {capacity: "16gb", brand: "G.Skill", dual_channel: true}
+
+
+
+computer_1 = "This computer has a mother board that costs #{motherboard[:price]} with a cpu that has #{cpu[:cores]} cores and #{ram[:capacity]} of ram."
+
+puts computer_1
